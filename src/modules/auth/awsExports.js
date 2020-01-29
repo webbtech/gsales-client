29dea461068ca5c0e58c3b524fb510420740548a
Ron Dyck <rond@webbtech.net>
2020-01-29 09:53:54 -0500

    clean up

--- src/modules/auth/awsExports.js
+++ src/modules/auth/awsExports.js
@@ -1,10 +1,3 @@
-/* const awsExports = {
-  Auth: {
-    region: 'us-east-1',
-    userPoolId: 'us-east-1_gsB59wfzW',
-    userPoolWebClientId: '2084ukslsc831pt202t2dudt7c',
-  },
-} */
 const awsExports = {
   Auth: {
     region: 'ca-central-1',
